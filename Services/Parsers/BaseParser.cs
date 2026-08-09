2c671c91cc01ff545caef8d9732eb5d21bb6408d
Jose M. Caballero (HP-i5) <josemcaba@gmail.com>
2026-08-09 23:59:17 +0200

    Mejorado Regex para NIF generico

--- Services/Parsers/BaseParser.cs
+++ Services/Parsers/BaseParser.cs
@@ -44,8 +44,7 @@ namespace FacturasApp.Services.Parsers
         /// Puede ser sobrescrita si se necesita un patrón específico.
         /// </summary>
         protected virtual Regex RegexNif { get; } = new(
-            // @"\b(?:ES|)((?:(?:[A-Z]|\d{2})(?:.|)\d{3}(?:.|)\d{3}(?:.|)[A-Z]|[A-Z](?:.|)\d{8}))\b",
-            @"\b(?:ES |)([A-Z](?:-|)\d{2}(?:\.|)\d{3}(?:\.|)\d{3}|\d{2}(?:\.|)\d{3}(?:\.|)\d{3}(?:-|)[A-Z]|[A-Z](?:-|)\d{7}(?:-|)[A-Z])\b",
+            @"\b(?:ES|)\s*((?:[A-Z][ -]{0,3}|)\d{1,2}(?:\.|)\d{3}(?:\.|)\d{3}(?:[ -]{0,3}[A-Z]|))\b",
             RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         // ── Helpers de extracción ────────────────────────────────────────────
